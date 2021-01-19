b46320190563f8d97ced2731e3b9708f96f3b12f
moonee <uuuu118@naver.com>
2021-01-19 17:00:02 +0900

    feat : add Trash / Home main contents

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import Templates from '../templates';
+import { Link } from 'react-router-dom';
+import Span from '../../components/atoms/Span';
+import styled from 'styled-components';
+
 const Home = () => {
   return (
     <Templates type="memos" pageName="Home">
-      <div>안뇽</div>
+      <Container>
+        <StyledLink to="/write">
+          <Span size="title">Write new Memo!✍️</Span>
+        </StyledLink>
+      </Container>
     </Templates>
   );
 };
 
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
 export default Home;
--- src/pages/Trash/index.tsx
+++ src/pages/Trash/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import Templates from '../templates';
+import Span from '../../components/atoms/Span';
+import styled from 'styled-components';
 
 const Trash = () => {
   return (
     <Templates type="trash" pageName="trash">
-      <div>쓰레기통</div>
+      <Container>
+        <Span size="title">Trash can♻️</Span>
+      </Container>
     </Templates>
   );
 };
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default Trash;
